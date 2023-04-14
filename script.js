f04e20c3a19d138d14e34e288f5208b8f321f32a
Walter Jose Serra <78517087+serrawalter1968@users.noreply.github.com>
2023-04-14 13:25:46 -0300

    Modelos y JS

--- script.js
+++ script.js
@@ -1,1 +1,82 @@
 //Javascriopt
+let productos = [
+  {
+    nombre: "Batistella",
+    Codigo: 001,
+    Stock: 10,
+    Precio: 50,
+  },
+
+  {
+    nombre: "Soy",
+    Codigo: 002,
+    Stock: 15,
+    Precio: 75,
+  },
+  {
+    nombre: "Sofisticacion",
+    Codigo: 003,
+    Stock: 10,
+    Precio: 70,
+  },
+  {
+    nombre: "Summun",
+    Codigo: 004,
+    Stock: 5,
+    Precio: 75,
+  },
+  {
+    nombre: "Styles",
+    Codigo: 005,
+    Stock: 15,
+    Precio: 50,
+  },
+  {
+    nombre: "Josefa",
+    Codigo: 006,
+    Stock: 10,
+    Precio: 40,
+  },
+  {
+    nombre: "Cristal",
+    Codigo: 011,
+    Stock: 15,
+    Precio: 50,
+  },
+  {
+    nombre: "Polity",
+    Codigo: 012,
+    Stock: 10,
+    Precio: 50,
+  },
+  {
+    nombre: "Letinas",
+    Codigo: 013,
+    Stock: 15,
+    Precio: 60,
+  },
+  {
+    nombre: "Kalo",
+    Codigo: 014,
+    Stock: 10,
+    Precio: 80,
+  },
+  {
+    nombre: "Cristaline",
+    Codigo: 015,
+    Stock: 10,
+    Precio: 70,
+  },
+  {
+    nombre: "Me fui",
+    Codigo: 016,
+    Stock: 11,
+    Precio: 90,
+  },
+  {
+    nombre: "Celestine",
+    Codigo: 017,
+    Stock: 15,
+    Precio: 100,
+  },
+];
